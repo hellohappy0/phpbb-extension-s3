1e1578b393b45beccaa116db90eaa7da645cd73f
hellohappy0 <45357888+hellohappy0@users.noreply.github.com>
2018-12-12 20:57:28 +0800

    Add files via upload

--- language/en/common.php
+++ language/en/common.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *
- * @package       phpBB Extension - S3
+ * @package       phpBB Extension - TencentCOS
  * @copyright (c) 2017 Austin Maddox
  * @license       http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
  *
@@ -18,29 +18,29 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, [
-	'ACP_S3'               => 'Settings',
-	'ACP_S3_SETTING_SAVED' => 'Settings have been saved successfully!',
+	'ACP_TencentCOS'               => 'Settings',
+	'ACP_TencentCOS_SETTING_SAVED' => 'Settings have been saved successfully!',
 
-	'ACP_S3_ACCESS_KEYS_EXPLAIN' => 'To successfully send requests to AWS APIs, you need to have a valid set of security credentials called
-	<strong>access keys</strong>. To learn more about AWS access keys, visit the <a href="https://aws.amazon.com/developers/access-keys/"
-		   target="_blank">About Access Keys page</a> in the developer documentation.',
+	'ACP_TencentCOS_ACCESS_KEYS_EXPLAIN' => 'To successfully send requests to Tencent APIs, you need to have a valid set of security credentials called
+	<strong>access keys</strong>. To learn more about Tencent access keys, visit the <a href="https://cloud.tencent.com/product/cos/document"
+		   target="_blank">developer documentation</a> .',
 
-	'ACP_S3_AWS_ACCESS_KEY_ID'         => 'AWS Access Key Id',
-	'ACP_S3_AWS_ACCESS_KEY_ID_EXPLAIN' => 'Enter your AWS Access Key Id for S3, e.g.: <samp>AKIAIOSFODNN7EXAMPLE</samp>.',
-	'ACP_S3_AWS_ACCESS_KEY_ID_INVALID' => '“%s” is not a valid AWS Access Key Id. It must be a combination of uppercase letters and/or numbers and exactly 20 characters.<br>Specifically, it must match the regular expression <code>/[A-Z0-9]{20})/</code>',
+	'ACP_TencentCOS_ACCESS_KEY_ID'         => 'Tencent Access Key Id',
+	'ACP_TencentCOS_ACCESS_KEY_ID_EXPLAIN' => 'Enter your Tencent Access Key Id for TencentCOS, e.g.: <samp>AKIAIOSqdf658FOd5D3N5qN7EwdXrewAMPLE</samp>.',
+	'ACP_TencentCOS_ACCESS_KEY_ID_INVALID' => '“%s” is not a valid Tencent Access Key Id. It must be a combination of letters and/or numbers and exactly 36 characters.<br>Specifically, it must match the regular expression <code>/[A-Za-z0-9]{36})/</code>',
 
-	'ACP_S3_AWS_SECRET_ACCESS_KEY'         => 'AWS Secret Access Key',
-	'ACP_S3_AWS_SECRET_ACCESS_KEY_EXPLAIN' => 'Enter your AWS Secret Access Key for S3, e.g.: <samp>wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY</samp>.',
-	'ACP_S3_AWS_SECRET_ACCESS_KEY_INVALID' => '“%s” is not a valid AWS Secret Access Key. It must be a combination of letters and/or numbers, forward slashes (/), plus signs (+), equals signs (=), and exactly 40 characters.<br>Specifically, it must match the regular expression <code>/[A-Za-z0-9\/+=]{40}/</code>',
+	'ACP_TencentCOS_SECRET_ACCESS_KEY'         => 'Tencent Secret Access Key',
+	'ACP_TencentCOS_SECRET_ACCESS_KEY_EXPLAIN' => 'Enter your Tencent Secret Access Key for TencentCOS, e.g.: <samp>vp88f3reGm29my0BPjyEWo5Qiv48dOSS</samp>.',
+	'ACP_TencentCOS_SECRET_ACCESS_KEY_INVALID' => '“%s” is not a valid Tencent Secret Access Key. It must be a combination of letters and/or numbers, and exactly 32 characters.<br>Specifically, it must match the regular expression <code>/[A-Za-z0-9]{32}/</code>',
 
-	'ACP_S3_REGION'         => 'AWS S3 Region',
-	'ACP_S3_REGION_EXPLAIN' => 'Enter the S3 region where your bucket resides, e.g.: <samp>us-west-2</samp>.',
-	'ACP_S3_REGION_INVALID' => 'You must enter a valid S3 region. For a list of the regions supported by Amazon S3, see the documentation here: http://docs.aws.amazon.com/general/latest/gr/rande.html.',
+	'ACP_TencentCOS_REGION'         => 'TencentCOS Region',
+	'ACP_TencentCOS_REGION_EXPLAIN' => 'Enter the TencentCOS region where your bucket resides, e.g.: <samp>ap-chengdu</samp>.',
+	'ACP_TencentCOS_REGION_INVALID' => 'You must enter a valid TencentCOS region. To learn more about Tencent access keys, visit the <a href="https://cloud.tencent.com/product/cos/document" target="_blank">developer documentation</a> .',
 
-	'ACP_S3_BUCKET'         => 'AWS S3 Bucket',
-	'ACP_S3_BUCKET_EXPLAIN' => 'Enter the name of your S3 bucket, e.g.: <samp>example-bucket</samp>. The bucket must already be created in your AWS account.',
-	'ACP_S3_BUCKET_INVALID' => 'You must enter a valid S3 bucket. For help working with Amazon S3 buckets, see the documentation here: http://docs.aws.amazon.com/AmazonS3/latest/dev/UsingBucket.html',
+	'ACP_TencentCOS_BUCKET'         => 'TencentCOS Bucket',
+	'ACP_TencentCOS_BUCKET_EXPLAIN' => 'Enter the name of your TencentCOS bucket, e.g.: <samp>example-1258617708</samp>. The bucket must already be created in your Tencent account.',
+	'ACP_TencentCOS_BUCKET_INVALID' => 'You must enter a valid TencentCOS bucket. To learn more about Tencent access keys, visit the <a href="https://cloud.tencent.com/product/cos/document" target="_blank">developer documentation</a> .',
 
-	'ACP_S3_IS_ENABLED'         => 'Is the extension enabled?',
-	'ACP_S3_IS_ENABLED_EXPLAIN' => 'Displays whether or not the extension is enabled and will use S3 for uploading and delivering your attachments.',
+	'ACP_TencentCOS_IS_ENABLED'         => 'Is the extension enabled?',
+	'ACP_TencentCOS_IS_ENABLED_EXPLAIN' => 'Displays whether or not the extension is enabled and will use TencentCOS for uploading and delivering your attachments.If you did not have this extension installed when you started using phpbb, before you use this extension, you need to copy all the contents of the file folder under the root path of phpbb(That is, the path where the files you downloaded are stored) into the bucket. If you do not do this, you may be prompted for errors when accessing attachments or pictures. This error reporting is not fatal, but you will not be able to access the previous files unless you stop the extension.',
 ]);
