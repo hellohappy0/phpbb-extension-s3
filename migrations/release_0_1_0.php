ee8f5993923cc06451c11d1740dda29192576730
hellohappy0 <45357888+hellohappy0@users.noreply.github.com>
2018-12-12 20:59:33 +0800

    Add files via upload

--- migrations/release_0_1_0.php
+++ migrations/release_0_1_0.php
@@ -1,39 +1,39 @@
 <?php
 /**
  *
- * @package       phpBB Extension - S3
+ * @package       phpBB Extension - TencentCOS
  * @copyright (c) 2017 Austin Maddox
  * @license       http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
  *
  */
 
-namespace AustinMaddox\s3\migrations;
+namespace AustinMaddox\tencentcos\migrations;
 
 class release_0_1_0 extends \phpbb\db\migration\migration
 {
 	public function update_data()
 	{
 		return [
-			['config.add', ['s3_aws_access_key_id', '']],
-			['config.add', ['s3_aws_secret_access_key', '']],
-			['config.add', ['s3_region', '']],
-			['config.add', ['s3_bucket', '']],
+			['config.add', ['tencentcos_aws_access_key_id', '']],
+			['config.add', ['tencentcos_aws_secret_access_key', '']],
+			['config.add', ['tencentcos_region', '']],
+			['config.add', ['tencentcos_bucket', '']],
 
 			[
 				'module.add',
 				[
 					'acp',
 					'ACP_CAT_DOT_MODS',
-					'ACP_S3_TITLE',
+					'ACP_TencentCOS_TITLE',
 				],
 			],
 			[
 				'module.add',
 				[
 					'acp',
-					'ACP_S3_TITLE',
+					'ACP_TencentCOS_TITLE',
 					[
-						'module_basename' => '\AustinMaddox\s3\acp\main_module',
+						'module_basename' => '\AustinMaddox\tencentcos\acp\main_module',
 						'modes'           => ['settings'],
 					],
 				],
--- migrations/release_1_0_2.php
+++ migrations/release_1_0_2.php
@@ -1,25 +1,25 @@
 <?php
 /**
  *
- * @package       phpBB Extension - S3
+ * @package       phpBB Extension - TencentCOS
  * @copyright (c) 2017 Austin Maddox
  * @license       http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
  *
  */
 
-namespace AustinMaddox\s3\migrations;
+namespace AustinMaddox\tencentcos\migrations;
 
 class release_1_0_2 extends \phpbb\db\migration\migration
 {
 	static public function depends_on()
 	{
-		return ['\AustinMaddox\s3\migrations\release_0_1_0'];
+		return ['\AustinMaddox\tencentcos\migrations\release_0_1_0'];
 	}
 
 	public function update_data()
 	{
 		return [
-			['config.add', ['s3_is_enabled', 0]],
+			['config.add', ['tencentcos_is_enabled', 0]],
 		];
 	}
 }
