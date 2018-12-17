9af477d758c50d9daa615d1dbf5dc96ef1c99f2b
hellohappy0 <45357888+hellohappy0@users.noreply.github.com>
2018-12-18 04:30:48 +0800

    Update main_listener.php

--- event/main_listener.php
+++ event/main_listener.php
@@ -3,6 +3,7 @@
  *
  * @package       phpBB Extension - TencentCOS
  * @copyright (c) 2017 Austin Maddox
+ * @copyright (c) 2018 ZWYang
  * @license       http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
  *
  */
@@ -92,12 +93,11 @@ class main_listener implements EventSubscriberInterface
 		if ($this->config['tencentcos_is_enabled'])
 		{
 			$filedata = $event['filedata'];
-
-			// Fullsize
-			$key = $filedata['physical_filename'];
-			$body = file_get_contents($this->phpbb_root_path . $this->config['upload_path'] . '/' . $key);
-			$uploadFileName = utf8_basename($filedata['real_filename']);
-			$this->uploadFileToTencentCOS($key, $body, $filedata['mimetype'], $uploadFileName );
+                        // Fullsize
+                        $key = $filedata['physical_filename'];
+                        $body = file_get_contents($this->phpbb_root_path . $this->config['upload_path'] . '/' . $key);
+                        $uploadFileName = utf8_basename($filedata['real_filename']);
+                        $this->uploadFileToTencentCOS($key ."_". $uploadFileName, $body, $filedata['mimetype'], $uploadFileName );
 		}
 	}
 
@@ -110,13 +110,19 @@ class main_listener implements EventSubscriberInterface
 	{
 		if ($this->config['tencentcos_is_enabled'])
 		{
-			foreach ($event['physical'] as $physical_file)
-			{
-				$this->tencentcos_client->deleteObject([
-					'Bucket' => $this->config['tencentcos_bucket'],
-					'Key'    => $physical_file['filename'],
-				]);
-			}
+			 foreach ($event['physical'] as $physical_file)
+                        {
+                                $uploadFileName = utf8_basename($physical_file['real_filename']);
+                                $this->tencentcos_client->deleteObject([
+                                        'Bucket' => $this->config['tencentcos_bucket'],
+                                        'Key'    => $physical_file['filename'] ."_". $uploadFileName,
+                                ]);
+                                //only the image have the thumbnail, but you can try to delete anyway.
+                                $this->tencentcos_client->deleteObject([
+                                        'Bucket' => $this->config['tencentcos_bucket'],
+                                        'Key'    => 'thumb_' . $physical_file['filename'] ."_". $uploadFileName,
+                                ]);
+                        }
 		}
 	}
 
@@ -132,36 +138,34 @@ class main_listener implements EventSubscriberInterface
 		if ($this->config['tencentcos_is_enabled'])
 		{
 			$block_array = $event['block_array'];
-			$attachment = $event['attachment'];
-
-			$key = 'thumb_' . $attachment['physical_filename'];
-			$tencentcos_link_thumb = '//' . $this->config['tencentcos_bucket'] . '.cos.ap-chengdu.myqcloud.com/' . $key;
-			$tencentcos_link_fullsize = '//' . $this->config['tencentcos_bucket'] . '.cos.ap-chengdu.myqcloud.com/' . $attachment['physical_filename'];
-			$local_thumbnail = $this->phpbb_root_path . $this->config['upload_path'] . '/' . $key;
-
-			if ($this->config['img_create_thumbnail'])
-			{
-
-				// Existence on local filesystem check. Just in case "Create thumbnail" was turned off at some point in the past and thumbnails weren't generated.
-				if (file_exists($local_thumbnail))
-				{
-					// Existence on TencentCOS check. Since this method runs on every page load, we don't want to upload the thumbnail multiple times.
-					try{
-						$this->tencentcos_client->headObject(['Bucket' => $this->config['tencentcos_bucket'], 'Key' => $key]);
-						//File has been here, nothing to do
-					} catch (\Exception $e) {
-						//No such file , Upload the thumbnail to TencentCOS.
-						$body = file_get_contents($local_thumbnail);
-						$uploadFileName = 'thumbnail_' . utf8_basename($attachment['real_filename']);
-                                                $this->uploadFileToTencentCOS($key, $body, $attachment['mimetype'], $uploadFileName);
-					}
-				}
-				$block_array['THUMB_IMAGE'] = $tencentcos_link_thumb;
-				$block_array['U_DOWNLOAD_LINK'] = $tencentcos_link_fullsize;
-			}
-
-			$block_array['U_INLINE_LINK'] = $tencentcos_link_fullsize;
-			$event['block_array'] = $block_array;
+                        $attachment = $event['attachment'];
+			
+                        $uploadFileName = utf8_basename($attachment['real_filename']);
+                        $key = 'thumb_' . $attachment['physical_filename'];
+                        
+			$tencentcos_link_thumb = '//' . $this->config['tencentcos_bucket'] . '.cos.ap-chengdu.myqcloud.com/' . $key ."_". $uploadFileName;
+                        $tencentcos_link_fullsize = '//' . $this->config['tencentcos_bucket'] . '.cos.ap-chengdu.myqcloud.com/' . $attachment['physical_filename'] ."_". $uploadFileName;
+                        $local_thumbnail = $this->phpbb_root_path . $this->config['upload_path'] . '/' . $key;
+                        if ($this->config['img_create_thumbnail'])
+                        {
+                                // Existence on local filesystem check. Just in case "Create thumbnail" was turned off at some point in the past and thumbnails weren't generated.
+                                if (file_exists($local_thumbnail))
+                                {
+                                        // Existence on TencentCOS check. Since this method runs on every page load, we don't want to upload the thumbnail multiple times.
+                                        try{
+                                                $this->tencentcos_client->headObject(['Bucket' => $this->config['tencentcos_bucket'], 'Key' => $key]);
+                                                //File has been here, nothing to do
+                                        } catch (\Exception $e) {
+                                                //No such file , Upload the thumbnail to TencentCOS.
+                                                $body = file_get_contents($local_thumbnail);
+                                                $this->uploadFileToTencentCOS($key ."_". $uploadFileName, $body, $attachment['mimetype'], 'thumbnail_' . $uploadFileName);
+                                        }
+                                }
+                                $block_array['THUMB_IMAGE'] = $tencentcos_link_thumb;
+                                $block_array['U_DOWNLOAD_LINK'] = $tencentcos_link_fullsize;
+                        }
+                        $block_array['U_INLINE_LINK'] = $tencentcos_link_fullsize;
+                        $event['block_array'] = $block_array;
 		}
 	}
 
